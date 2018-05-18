a2cf38f716c5baba4ade08cdbc404ebc17efb11f
BoteWu <wushaoliang183@126.com>
2018-05-18 18:09:23 +0800

    Add files via upload

---
+++ CommonHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCMS
+{
+    public class CommonHelper
+    {
+        public static T GetConfigValue<T>(string hashKey, T defaultValue)
+        {
+            try
+            {
+                var value = ConfigurationManager.AppSettings[hashKey];
+                if (value == null)
+                    return defaultValue;
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
